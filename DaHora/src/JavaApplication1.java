0a3fdfbf22b6c5454e0d76352c722f88fd2fe967
Rajha Fajardo <rajhafajardo@hotmail.com>
2019-04-30 12:18:20 -0300

    correção menu

--- DaHora/src/JavaApplication1.java
+++ DaHora/src/JavaApplication1.java
@@ -2,57 +2,19 @@
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JavaApplication1 {
 
 
     
    public static void main (String arg []){
-		Scanner ler = new Scanner(System.in);
-        // Conteudo
-
-		System.out.printf("Informe Seu nome:\n");
-		String content = ler.nextLine();
-		        try {
-
-            // Cria arquivo
-            File file = new File("teste.txt");
-
-            // Se o arquivo nao existir, ele gera
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            // Prepara para escrever no arquivo
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            // Escreve e fecha arquivo
-            try (BufferedWriter bw = new BufferedWriter(fw)) {
-                // Escreve e fecha arquivo
-                bw.write(content);
-            }
-            
-            // Le o arquivo
-            FileReader ler1 = new FileReader("teste.txt");
-            BufferedReader reader = new BufferedReader(ler1);  
-            String linha;
-            while( (linha = reader.readLine()) != null ){
-                System.out.println(linha);
-            }
-
-            // Imprime confirmacao
-            System.out.println("Feito =D");
-
-        } catch (IOException e) {
-        }
-    }
-                     menu();
-		System.out.println("Programa encerrado.");
-	//  menu principal
-	private static void menu() { // menu principal
+         // menu principal
 		int opcao = 0;
 		do {
-			System.out
-					.println("\n\n### DaHora - Banco de Horas ###");
+			
+                        System.out.println("              \n\n#### DaHora - Banco de Horas  ####");
 			System.out.println("\n                  ================================");
 			System.out.println("                  |     1 - Cadastro Funcionário   |");
 			System.out.println("                  |     2 - Cadastro Empresa       |");
@@ -90,20 +52,8 @@ public class JavaApplication1 {
 			}
 		} while (opcao != 0);
 	}
-
-    private static void CadastroFuncionario() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static void CadastroEmpresa() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static void ConsultaFuncionario() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static void ConsultaEmpresa() {
+   	
+    private static void FazerCheckOut() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -111,8 +61,84 @@ public class JavaApplication1 {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private static void FazerCheckOut() {
+    private static void ConsultaEmpresa() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    private static void CadastroEmpresa() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-}
+
+    private static void CadastroFuncionario() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void ConsultaFuncionario() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+       
+       
+		Scanner ler = new Scanner(System.in);
+                // Conteudo
+		System.out.printf("Informe Seu nome:\n");
+		String content = ler.nextLine();
+		        try {
+
+            // Cria arquivo
+            File file = new File("teste.txt");
+
+            // Se o arquivo nao existir, ele gera
+            if (!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException ex) {
+                    Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            // Prepara para escrever no arquivo
+            FileWriter fw;
+        try {
+            fw = new FileWriter(file.getAbsoluteFile());
+        } catch (IOException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            // Escreve e fecha arquivo
+            try (BufferedWriter bw = new BufferedWriter(fw)) {
+                try {
+                    // Escreve e fecha arquivo
+                    bw.write(content);
+                } catch (IOException ex) {
+                    Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } catch (IOException ex) {
+           Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+       }
+            
+            // Le o arquivo
+            FileReader ler1 = null;
+        try {
+            ler1 = new FileReader("teste.txt");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            BufferedReader reader = new BufferedReader(ler1);  
+            String linha;
+       try {
+           while( (linha = reader.readLine()) != null ){
+               System.out.println(linha);
+           }
+       } catch (IOException ex) {
+           Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+       }
+
+            // Imprime confirmacao
+            System.out.println("Feito =D");
+
+        } catch (IOException e) {
+        }
+    }
+                    
+
+    
+    
