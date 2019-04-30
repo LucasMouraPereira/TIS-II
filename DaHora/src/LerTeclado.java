a525ae6a774a289baafc8008de54c855f7a4cc37
Rajha Fajardo <rajhafajardo@hotmail.com>
2019-04-30 11:47:36 -0300

    Criei o menu.
    Sugeri os nomes dos métodos, fiquem a vontade para mudar

--- DaHora/src/LerTeclado.java
+++ DaHora/src/LerTeclado.java
@@ -3,14 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-  import java.util.Scanner;
+
 /**
  *
  * @author rajha
  */
 public class LerTeclado {
- 
-Scanner ler = new Scanner(System.in);
-System.out.printf("Informe Seu nome:\n");
-String = ler.nextLine(); 
+
 }
--- DaHora/src/javaapplication1/JavaApplication1.java
+++ DaHora/src/javaapplication1/JavaApplication1.java
@@ -7,15 +7,16 @@ public class JavaApplication1 {
 
 
     
-    public static void main(String[] args) {
-        
-        Scanner ler = new Scanner(System.in);
-        System.out.printf("Informe Seu nome:\n");
-        String String = ler.nextLine(); 
-        
-        try {
-            // Conteudo
-            //String content = "Rajha";
+    public static void main (String arg []){
+		Scanner ler = new Scanner(System.in);
+        // Conteudo
+
+		System.out.printf("Informe seu nome:\n");
+		String content = ler.nextLine();
+                System.out.printf("Informe seu CPF:\n");
+		String content = ler.nextLine();
+                
+		        try {
 
             // Cria arquivo
             File file = new File("teste.txt");
@@ -29,14 +30,13 @@ public class JavaApplication1 {
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             // Escreve e fecha arquivo
             try (BufferedWriter bw = new BufferedWriter(fw)) {
-                char[] content = null;
                 // Escreve e fecha arquivo
                 bw.write(content);
             }
             
             // Le o arquivo
-            FileReader ler = new FileReader("teste.txt");
-            BufferedReader reader = new BufferedReader(ler);  
+            FileReader ler1 = new FileReader("teste.txt");
+            BufferedReader reader = new BufferedReader(ler1);  
             String linha;
             while( (linha = reader.readLine()) != null ){
                 System.out.println(linha);
@@ -47,5 +47,74 @@ public class JavaApplication1 {
 
         } catch (IOException e) {
         }
+                        menu();
+		System.out.println("Programa encerrado.");
+	//  menu principal
+	private static void menu() { // menu principal
+		int opcao = 0;
+		do {
+			System.out
+					.println("\n\n### DaHora - Banco de Horas ###");
+			System.out.println("\n                  ================================");
+			System.out.println("                  |     1 - Cadastro Funcionário   |");
+			System.out.println("                  |     2 - Cadastro Empresa       |");
+			System.out.println("                  |     3 - Consulta Funcionário   |");
+			System.out.println("                  |     4 - Consulta Empresa       |");
+			System.out.println("                  |     5 - Fazer Check in         |");
+			System.out.println("                  |     6 - Fazer Check out        |");
+			System.out.println("                  |     0 - Sair                   |");
+			System.out.println("                  =================================\n");
+			System.out.println                    ("Opção -> ");
+			System.out.print("\n");
+			switch (opcao) {
+			case 1: CadastroFuncionario();
+				break;
+			case 2:
+				CadastroEmpresa ();
+				break;
+                        case 3:
+				ConsultaFuncionario ();
+				break;
+                        case 4:
+				ConsultaEmpresa ();
+				break;
+                        case 5:
+				FazerCheckIn();
+				break;
+                        case 6:
+				FazerCheckOut();
+				break;        
+			case 0:
+				break;
+			default:
+				System.out.println("Opção Inválida!");
+				break;
+			}
+		} while (opcao != 0);
+	}
+
+    private static void CadastroFuncionario() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void CadastroEmpresa() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void ConsultaFuncionario() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void ConsultaEmpresa() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void FazerCheckIn() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void FazerCheckOut() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     }
 }
